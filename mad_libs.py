228696cb5a7512e9abf256337d23a2435f5d7166
Kevin <kevin@pop-os.localdomain>
2019-08-29 15:28:29 -0700

    First run of mad_libs.py and included default example.txt

--- mad_libs.py
+++ mad_libs.py
@@ -1,30 +1,45 @@
-class MadLibs(text, shuffle = False):
-    
+import random
+import re
+
+from collections import defaultdict
+
+
+
+
+class MadLibs:
+  
+    def __init__(self, text, shuffle=True):
+        tags = self.get_tags(text)
+
+        responses = defaultdict(lambda: [])
+
+        for tag in tags:
+            responses[tag].append(self.ask_pos(tag))
+
+        if shuffle:
+            for key in responses.keys():  # Maybe call key, pos
+                random.shuffle(responses[key])
+
+        for tag in tags:
+            tag_escaped = re.escape('[' + tag + ']')
+            word = responses[tag].pop()
+
+            text = re.sub(tag_escaped, word, text, count=1)
+
+        print(text)
+
+
+    @staticmethod
     def get_tags(string):
         return re.findall(r'(?<=\[).+?(?=\])', string)
-    
+
+    @staticmethod
     def ask_pos(pos):  # Parts Of Speech
         return input(f'Enter a(n): {pos}\n')
-    
-    tags = get_tags(text)
 
-    responses = defaultdict(lambda: [])
-    
-    for tag in tags:
-        responses[tag].append(ask_pos(tag))
 
-    if shuffle:
-        for key in responses.keys():  # Maybe call key, pos
-            random.shuffle(responses[key])
-
-    for tag in tags:
-        tag_escaped = re.escape('[' + tag + ']')
-        word = responses[tag].pop()
-        
-        text = re.sub(tag_escaped, word, text, count=1)
-    
-	print(text)
 
 if __name__ == '__main__':
-	text = open('example.txt')
-	m = MadLibs(text)
+    text = open('example.txt').read()
+    m = MadLibs(text)
+
