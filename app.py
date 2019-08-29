f4cc22e447957e7c5c92d86fe26ba8e50d0b4968
Kevin <kevin@pop-os.localdomain>
2019-08-29 16:47:55 -0700

    Started setting up a form page.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request
+
 
 from mad_libs import MadLibs
 
@@ -10,7 +11,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def test():
-	return m.run()
+def form():
+	return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def form_post():
+	text = request.form['text']
+	processed_text = text.upper()
+	return processed_text
 
 
+if __name__ == '__main__':
+	app.run()
